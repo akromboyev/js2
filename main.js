d38ce5793111758e1412f8d459d4656e8da9bd00
Dilshod <dilshodakromboyev@gmail.com>
2024-10-14 14:31:58 +0500

    finish

---
+++ main.js
@@ -0,0 +1,7 @@
+
+let nok = prompt("nok narxini kiriting")
+let banan = prompt("banan narxini kiriting")
+let shaftoli = prompt("shaftoli narxini kiriting")
+let urik = prompt("urik narxini kiriting")
+let qovun = prompt("qovun narxini kiriting")
+let sum =parseInt(nok) +parseInt(banan)+parseInt(shaftoli)+parseInt(urik)+parseInt(qovun)
